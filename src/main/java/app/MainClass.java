1ec2c1edf0999cc18613cc1e605cbe96133be511
Vlad <vladmale@mail.ru>
2017-02-15 10:53:51 +0500

    Созданы классы всех сущьностей

--- src/main/java/app/MainClass.java
+++ src/main/java/app/MainClass.java
@@ -4,27 +4,17 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import com.mashape.unirest.request.GetRequest;
-import com.mashape.unirest.request.HttpRequest;
-import com.mashape.unirest.request.HttpRequestWithBody;
 
-/**
- * Created by rootid on 13.02.2017.
- */
+
 public class MainClass {
     public static void main(String[] args) throws UnirestException {
 
 
 
-        HttpResponse<JsonNode> jsonResponse = Unirest.post("http://httpbin.org/post")
-                .header("accept", "application/json")
-                .queryString("apiKey", "123")
-                .field("parameter", "value")
-                .field("foo", "bar")
+
+
+        HttpResponse<JsonNode> jsonResponse = Unirest.get("http://swapi.co/api/")
                 .asJson();
-
-
-
 
 
 
@@ -40,17 +30,6 @@ public class MainClass {
         System.out.print("body: ");
         System.out.println(jsonResponse.getBody());
 
-
-////        HttpRequestWithBody request = Unirest.post("http://httpbin.org/people");
-//
-//        HttpResponse response = Unirest.get("http://httpbin.org/people")
-//                .header("accept", "application/json").asString();
-//
-//
-////        HttpResponse<JsonNode> jsonResponse = Unirest.post("http://httpbin.org/people").asJson();
-//        System.out.println(response);
-//
-//
 
 
 
---
+++ src/main/java/models/Starship.java
@@ -0,0 +1,7 @@
+package models;
+
+/**
+ * Created by vgorokhov on 15.02.2017.
+ */
+public class Starship {
+}
