26c85a46bdb7b058f64a45c1829c1d4b02d84959
Vlad <vladmale@mail.ru>
2017-02-19 13:33:12 +0500

    переместил построение людей в функцию bildPeople класса BildModels

--- src/main/java/app/MainClass.java
+++ src/main/java/app/MainClass.java
@@ -8,55 +8,13 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import deserialize.PeopleDeserializer;
 import models.Arrays.People;
 import models.Human;
+import utils.BildModels;
 
 public class MainClass {
     public static void main(String[] args) throws UnirestException {
 
-
-
-
-
-        String url = "http://swapi.co/api/people/";
-        String next = "First Start";
-        People people = new People();
-
-        while (next != "") {
-            HttpResponse<JsonNode> jsonResponse = Unirest.get(url)
-                    .asJson();
-
-//            System.out.print("status code: ");
-//            System.out.println(jsonResponse.getStatus());
-//
-//            System.out.print("status text: ");
-//            System.out.println(jsonResponse.getStatusText());
-//
-//            System.out.print("headers: ");
-//            System.out.println(jsonResponse.getHeaders());
-//
-            System.out.print("body: ");
-            System.out.println(jsonResponse.getBody());
-            String responseJsonString = jsonResponse.getBody().toString();
-
-            JsonElement jsonElementParse = new JsonParser().parse(responseJsonString);
-            JsonObject jsonObjectParse = jsonElementParse.getAsJsonObject();
-
-//            System.out.println(jsonObjectParse.get("next").getAsString());
-
-            if (jsonObjectParse.get("next").isJsonNull()){
-                next = "";
-            }
-            else {
-                next = jsonObjectParse.get("next").getAsString();
-                url = next;
-            }
-
-
-            Gson gson = new GsonBuilder()
-                    .registerTypeAdapter(People.class, new PeopleDeserializer())
-                    .create();
-            people = gson.fromJson(responseJsonString, People.class);
-        }
-
+        BildModels bildModels = new BildModels();
+        People people = bildModels.bildPeople();
 
         Human human = people.getPeople("http://swapi.co/api/people/88/");
         System.out.println(human.getName());
--- src/main/java/utils/BildModels.java
+++ src/main/java/utils/BildModels.java
@@ -1,7 +1,50 @@
 package utils;
 
+import com.google.gson.*;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import deserialize.PeopleDeserializer;
+import models.Arrays.People;
+
 /**
  * Created by rootid on 19.02.2017.
  */
 public class BildModels {
+
+
+    public People bildPeople() throws UnirestException {
+        String url = "http://swapi.co/api/people/";
+        String next = "First Start";
+        People people = new People();
+
+        while (next != "") {
+            HttpResponse<JsonNode> jsonResponse = Unirest.get(url)
+                    .asJson();
+            System.out.print("body: ");
+            System.out.println(jsonResponse.getBody());
+            String responseJsonString = jsonResponse.getBody().toString();
+
+            JsonElement jsonElementParse = new JsonParser().parse(responseJsonString);
+            JsonObject jsonObjectParse = jsonElementParse.getAsJsonObject();
+
+
+            if (jsonObjectParse.get("next").isJsonNull()){
+                next = "";
+            }
+            else {
+                next = jsonObjectParse.get("next").getAsString();
+                url = next;
+            }
+
+
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(People.class, new PeopleDeserializer())
+                    .create();
+            people = gson.fromJson(responseJsonString, People.class);
+        }
+        return people;
+    }
+
 }
