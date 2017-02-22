ffb8dfb38340117d4f233321229020ba38211acc
labswork <vgorokhov57@gmail.com>
2017-02-22 07:58:35 +0500

    Update Singlton.java

--- src/main/java/models/Singlton.java
+++ src/main/java/models/Singlton.java
@@ -4,9 +4,5 @@ package models;
  * Created by vgorokhov on 15.02.2017.
  */
 public class Singlton {
-//    private String type;
-//
-//    Singlton(String type){
-//        this.type = type;
-//    }
+//github commit
 }
